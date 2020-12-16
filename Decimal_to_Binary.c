a669278f0305273faeb6ed5ff58856210371db7a
BlockWolfs <1390597476@qq.com>
2020-12-16 10:13:38 +0800

    add funation that octal number transform hexadecimal number

--- Decimal_to_Binary.c
+++ Decimal_to_Binary.c
@@ -9,11 +9,31 @@ int D_to_B(int decimal_number)
 	return 0;
 }
 
+int O_to_H(int octal_number)
+{
+	int addition=0,power=1;
+	addition = octal_number%10*power;
+	octal_number /= 10;
+	while( octal_number != 0)
+	{
+		power *= 8;
+		addition += octal_number%10*power;
+		octal_number /= 10;
+
+	}
+	printf("The hexadecimal numbe is %X",addition);
+}
+
 int main(void)
 {
-	int decimal_number;
-	scanf("%d",&decimal_number);
-	D_to_B(decimal_number);
+	//int decimal_number;
+	//scanf("%d",&decimal_number);
+	//D_to_B(decimal_number);
+	
+	int octal_number;
+	scanf("%d",&octal_number);
+	O_to_H(octal_number);
+
 	return 0;
 }
 
